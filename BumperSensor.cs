9f22422b3326245e9d0d02860c93e95dd1345435
Tompi <robcio@bylica.com>
2012-08-17 14:03:51 +0200

    Implements simple driver connection

---
+++ BumperSensor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Dss.Core.Attributes;
+
+
+using bumper = Microsoft.Robotics.Services.ContactSensor.Proxy;
+using Microsoft.Ccr.Core;
+using Microsoft.Dss.Core;
+namespace com.bylica.robcio
+{
+    class BumperSensor
+    {
+
+
+
+        private RobcioService robcioService;
+
+
+        public void initBumper(RobcioService rs, bumper.ContactSensorArrayOperations _bumperPort)
+        {
+            this.robcioService = rs;
+            // Create the bumper notification port.
+            bumper.ContactSensorArrayOperations bumperNotificationPort = new bumper.ContactSensorArrayOperations();
+
+            // Subscribe to the bumper service, receive notifications on the bumperNotificationPort.
+            _bumperPort.Subscribe(bumperNotificationPort);
+
+            // Start listening for updates from the bumper service.
+            robcioService.Activate(
+                Arbiter.Receive<bumper.Update>
+                    (true, bumperNotificationPort, BumperHandler));
+
+
+        }
+
+
+        private void BumperHandler(bumper.Update notification)
+        {
+
+
+            if (notification.Body.Pressed)
+            {
+                robcioService.writeToLogInfo("------------------------------");
+                robcioService.writeToLogInfo("Ouch - the bumper was pressed.");
+                robcioService.writeToLogInfo("------------------------------");
+            }
+        }
+
+
+
+
+    }
+}
--- Robcio.cs
+++ Robcio.cs
@@ -7,6 +7,13 @@ using Microsoft.Dss.ServiceModel.Dssp;
 using Microsoft.Dss.ServiceModel.DsspServiceBase;
 using W3C.Soap;
 
+using Microsoft.Dss.Core;
+
+using bumper = Microsoft.Robotics.Services.ContactSensor.Proxy;
+using drive = Microsoft.Robotics.Services.Drive.Proxy;
+using motor = Microsoft.Robotics.Services.Motor.Proxy;
+
+
 namespace com.bylica.robcio
 {
 	[Contract(Contract.Identifier)]
@@ -19,20 +26,72 @@ namespace com.bylica.robcio
 		
 		[ServicePort("/Robcio", AllowMultipleInstances = true)]
 		RobcioOperations _mainPort = new RobcioOperations();
-		
-		public RobcioService(DsspServiceCreationPort creationPort)
-			: base(creationPort)
-		{
-		}
-		
-		protected override void Start()
-		{
-			
-			// 
-			// Add service specific initialization here
-			// 
-			
+
+        #region DriveRegion
+
+        [Partner("Drive",
+        Contract = drive.Contract.Identifier,
+        CreationPolicy = PartnerCreationPolicy.UseExisting)]
+        drive.DriveOperations _drivePort = new drive.DriveOperations();
+
+        #endregion
+
+        #region BumperRegion
+
+
+        [Partner("bumper", Contract = bumper.Contract.Identifier,
+        CreationPolicy = PartnerCreationPolicy.UseExisting)]
+        private bumper.ContactSensorArrayOperations _bumperPort = new bumper.ContactSensorArrayOperations();
+
+
+        #endregion
+
+
+
+
+        public RobcioService(DsspServiceCreationPort creationPort)
+            : base(creationPort)
+        {
+        }
+
+      
+
+
+        public void writeToLogInfo(String msg) {
+            LogInfo(LogGroups.Console, msg);
+        }
+        public void writeToLogError(String msg)
+        {
+            LogError(LogGroups.Console, msg);
+        }
+
+        public Port<DateTime> getTimeoutPort(int time) {
+
+            
+            return TimeoutPort(time);
+        }
+
+
+
+        public void getSpawnIterator<T0>(T0 t0, IteratorHandler<T0> handler) {
+
+
+
+            SpawnIterator<T0>(t0, handler);
+
+        }
+
+        public void getSpawnIterator<T0, T1>(T0 t0, T1 t1, IteratorHandler<T0, T1> handler) {
+
+            SpawnIterator<T0,T1>(t0,t1, handler);
+        }
+
+
+        protected override void Start()
+		{						
 			base.Start();
+            new BumperSensor().initBumper(this,_bumperPort);
+            new MotoDrives(this, _drivePort).test();
 		}
 	}
 }
